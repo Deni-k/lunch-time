587bba47da74856b08590b86eaff0d36c82d725d
deni_k <deniz.kesmez@rwth-aachen.de>
2020-11-02 13:58:22 +0100

    Initial commit, setting up the environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppComponent } from './app.component';
+import {HomeModule} from './pages/home/home.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
