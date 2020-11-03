fbd8cc3544e52c22329cd2ce9b63617715c5bca4
deni_k <deniz.kesmez@rwth-aachen.de>
2020-11-03 17:43:03 +0100

    feature: storing form in local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'lunchTime';
+export class AppComponent implements OnInit{
+  public title = 'lunchTime';
+  public counter = '';
+  public ngOnInit(): void{
+    if (!window.localStorage.getItem('counter')){
+      window.localStorage.setItem('counter', '0');
+    }
+    this.counter = window.localStorage.getItem('counter');
+  }
 }
